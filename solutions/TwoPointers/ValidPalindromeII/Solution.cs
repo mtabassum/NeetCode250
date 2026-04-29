18e1cd562b66910b78fd31384157e10ef17d4da2
mtabassum <mtabassum015@gmail.com>
2026-04-29 15:55:09 -0500

    Add Valid Palindrome II solution

---
+++ solutions/TwoPointers/ValidPalindromeII/Solution.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TwoPointers.ValidPalindromeII
+{
+    public class Solution
+    {
+        public bool ValidPalindrome(string s)
+        {
+            int left = 0, right = s.Length - 1;
+
+            while(left < right)
+            {
+                if (s[left] != s[right])
+                {
+                    string skipLeft = s.Substring(left+1, right-left);
+                    string skipRight = s.Substring(left, right - left);
+                    return IsPalindrome(skipLeft) || IsPalindrome(skipRight);
+                }
+                left++;
+                right--;
+            }
+            return true;
+         }
+
+        public bool IsPalindrome(string s)
+        {
+            int left = 0, right = s.Length - 1;
+
+            while (left < right)
+            {
+                if (s[left] != s[right])
+                {
+                    return false;
+                }
+                left++;
+                right--;
+            }
+            return true;
+        }
+    }
+}
---
+++ tests/TwoPointers.Tests/ValidPalindromeII/ValidPalindromeIITests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TwoPointers.ValidPalindromeII;
+
+namespace TwoPointers.Tests.ValidPalindromeII
+{
+    [TestFixture]
+    public class ValidPalindromeIITests
+    {
+        private Solution _solution;
+
+        [SetUp]
+        public void Setup()
+        {
+            _solution = new Solution();
+        }
+
+        [Test]
+        [TestCase("aca", ExpectedResult = true)]
+        [TestCase("abbadc", ExpectedResult = false)]
+        [TestCase("abbda", ExpectedResult = true)]
+
+        public bool IsPalindrome_Tests(string input)
+        {
+           return _solution.ValidPalindrome(input);
+        }
+    }
+}
